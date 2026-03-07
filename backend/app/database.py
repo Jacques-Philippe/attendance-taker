7c9e0c3b2fa6cce4d7f6944f9f22cda375aacd12
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-03-07 08:05:34 -0500

    Update imports

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from .config import settings
-
 
 engine = create_engine(settings.database_url, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
