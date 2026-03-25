180b8e1b2838c0cdeaefd41e261a30f3e90ba8bb
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-03-25 15:39:17 -0400

    Fix errors

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -21,6 +21,7 @@ const router = createRouter({
       component: () => import("../views/RegisterView.vue"),
     },
     {
+      path: "",
       component: AppLayout,
       children: [
         {
